9c009ef84dc8bcd52d598d70e798a6778b0e38f9
Arya Aji <72726996+arya-aji@users.noreply.github.com>
2026-06-08 09:50:19 +0700

    feat: add projects page with slider and filterable grid view

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -311,9 +311,9 @@
   </div>
 </main>
 
-{#if hoveredProject && (hoveredProject.gif || hoveredProject.image)}
+{#if hoveredProject && (hoveredProject.banner || hoveredProject.gif)}
   <!-- svelte-ignore a11y_no_static_element_interactions -->
-  <div class="floating-video-popup" style="--x: {mouseX + 20}px; --y: {mouseY + 20}px; background-image: url('{hoveredProject.gif || hoveredProject.image}');"></div>
+  <div class="floating-video-popup" style="--x: {mouseX + 20}px; --y: {mouseY + 20}px; background-image: url('{hoveredProject.banner || hoveredProject.gif}');"></div>
 {/if}
 
 {#if caseStudyProject}
